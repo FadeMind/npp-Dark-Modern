bec694f221e5040de83a7f3a11708e44ff768389
FadeMind <fademind@gmail.com>
2026-06-03 20:16:13 +0200

    autohotkey-dark.xml

--- install.cmd
+++ install.cmd
@@ -1,16 +1,33 @@
 @echo off
+
+net session >nul 2>&1
+if %errorLevel% equ 0 (
+    echo Run as a regular user, not as an administrator!
+    pause
+    exit /b 1
+)
+
 :: ==========================================================
 :: Install Dark+ Modern notepad++ theme
 :: ==========================================================
 :: Data: 20/05/2026
 :: Autor: helldio
+::
+:: ==========================================================
+:: Changes 
+:: ==========================================================
+:: - Don't run as admin guard
+:: - autohotkey-dark.xml | 03/06/2026 FadeMind
 
 setlocal
 
 set "THEME_NAME=Dark+ Modern.xml"
 set "THEME_DIR=%AppData%\Notepad++\themes"
 
-set "UDL_SRC=plugins\markdown-dark.xml"
+set "UDL_MDN=plugins\markdown-dark.xml"
+set "UDL_DIR=%AppData%\Notepad++\userDefineLangs"
+
+set "UDL_AHK=plugins\autohotkey-dark.xml"
 set "UDL_DIR=%AppData%\Notepad++\userDefineLangs"
 
 set "PLUGIN_SRC=plugins\CSVLint.xml"
@@ -31,8 +48,16 @@ if %errorlevel% neq 0 ( echo   [FAILED] Theme & set "ERRORS=1" ) else echo   [OK
 :: ----------------------------------------------------------
 echo Installing UDL files...
 if not exist "%UDL_DIR%" mkdir "%UDL_DIR%"
-copy /y "%~dp0%UDL_SRC%" "%UDL_DIR%\" >nul
-if %errorlevel% neq 0 ( echo   [FAILED] %UDL_SRC% & set "ERRORS=1" ) else echo   [OK] %UDL_SRC% ^-^> %UDL_DIR%
+copy /y "%~dp0%UDL_MDN%" "%UDL_DIR%\" >nul
+if %errorlevel% neq 0 ( echo   [FAILED] %UDL_MDN% & set "ERRORS=1" ) else echo   [OK] %UDL_MDN% ^-^> %UDL_DIR%
+
+:: ----------------------------------------------------------
+:: UDL (Autohotkey)
+:: ----------------------------------------------------------
+echo Installing UDL files...
+if not exist "%UDL_DIR%" mkdir "%UDL_DIR%"
+copy /y "%~dp0%UDL_AHK%" "%UDL_DIR%\" >nul
+if %errorlevel% neq 0 ( echo   [FAILED] %UDL_AHK% & set "ERRORS=1" ) else echo   [OK] %UDL_AHK% ^-^> %UDL_DIR%
 
 :: ----------------------------------------------------------
 :: Plugin config (CSVLint)
